23489ea840da15148db8ce7850fd805e15010afd
fawoyeye <106695238+fawoyeye@users.noreply.github.com>
2022-08-05 22:51:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+var slid = document.querySelector('.slide');
 var span = document.querySelector('span');
 var p = document.querySelector('.talk');
 var body = document.querySelector('body');
@@ -14,13 +14,12 @@ var strategy =document.querySelector('#strategy');
 var circleOne = document.querySelector('#circle1');
 var circleTwo = document.querySelector('#circle2');
 var circleThree = document.querySelector('#circle3');
-
+var mobilenav = document.querySelector('.mobilenav');
 p.onclick = function(){
 span.classList.toggle('see');}
 
 
-open.onclick = function(){
-			mobilenav.classList.toggle('show');}
+
 
 
 
@@ -36,14 +35,19 @@ var num = -1;
   
 				if(num<0){num = arr.length-1};
 					arr[num].style.display="flex";
+					arr[num].style.opacity="1";
 					arr[num].style.zIndex="1";
+					color.classList.add('slidein');
+				strategy.classList.add('slidein');
+				problems.classList.add('slidein');
 					setTimeout(function(){
 			arr[num].style.display="none";		
+			arr[num].style.opacity="0";
 	},2000);
-	if(num == 0){circleOne.style.background="grey"};
-if(num == 1){circleTwo.style.background="grey"};
-if(num == 2){circleThree.style.background="grey"};
-
+	
+if(num == 0){circleOne.style.background="grey";circleTwo.style.background="orange";circleThree.style.background="orange";};
+if(num == 1){circleTwo.style.background="grey";circleOne.style.background="orange";circleThree.style.background="orange";};
+if(num == 2){circleThree.style.background="grey";circleTwo.style.background="orange";circleOne.style.background="orange";};
 
 
 		};
@@ -57,8 +61,13 @@ if(num == 2){circleThree.style.background="grey"};
  if(num >= arr.length){num = 0};	
  	arr[num].style.display="flex";
  	arr[num].style.zIndex="1";
+ 	arr[num].style.opacity="1";
+ 	color.classList.add('slidein');
+				strategy.classList.add('slidein');
+				problems.classList.add('slidein');
 setTimeout(function(){
 			arr[num].style.display="none";		
+			arr[num].style.opacity="0";
 	},2000);
 	if(num == 0){circleOne.style.background="grey";circleTwo.style.background="orange";circleThree.style.background="orange";};
 if(num == 1){circleTwo.style.background="grey";circleOne.style.background="orange";circleThree.style.background="orange";};
@@ -71,19 +80,39 @@ setInterval(forward,4000);
 
 circleOne.onclick = function(){
 				color.style.display="flex";
+				color.style.opacity="1";
 				strategy.style.display="none";
 				problems.style.display="none";
+				color.classList.add('slidein');
+				strategy.classList.add('slidein');
+				problems.classList.add('slidein');
 };
 
 
 circleTwo.onclick = function(){
 				color.style.display="none";
 				strategy.style.display="none";
+				problems.style.opacity="1";
 				problems.style.display="flex";
+				color.classList.add('slidein');
+				strategy.classList.add('slidein');
+				problems.classList.add('slidein');
 };
 
 circleThree.onclick = function(){
 				color.style.display="none";
 				strategy.style.display="flex";
+				straregy.style.opacity="1";
 				problems.style.display="none";
-}
+				color.classList.add('slidein');
+				strategy.classList.add('slidein');
+				problems.classList.add('slidein');
+};
+
+
+var w = window.innerWidth;
+
+
+if(w > 360) {slid.classList.remove('slidein');
+				
+};
